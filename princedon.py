a18e71e6a4b19af0422c2d45982a04bc4d08596c
princeraaz1 <75932324+princeraaz1@users.noreply.github.com>
2020-12-13 18:34:03 +0545

    Update and rename zeroboy.py to princedon.py

--- princedon.py
+++ princedon.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+|__(~)    •||•HACKER - PRINCE -YADAV------•◈•
+|__\~~) •||•RANA - PRINCE---------------•◈•
+|__(-----\  •◈•------HACKER-PRINCE--------•◈•
+|__~~~\ •◈•-----█-------P-------█------•◈•
+|__~~~\ •◈•-----█-------K-------█------•◈•
+|__~~~\ •◈•-----█-------Y-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mPRINCE YADAV
+\033[1;96mInstagram \033[1;93m: \033[1;FlowPRINCE
+\033[1;96mFacebook  \033[1;93m: \033[1; Prince Kumar Yadav
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/PrinceHacker/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRana PRINCE
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowPRINCE
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Prince Kumar Yadav
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/PrinceHacker/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "prince"
+CorrectPassword = "prince"
 
 loop = 'true'
 while (loop == 'true'):
